4d3606f0da106c33479cb3aa966908e48214c409
Naeem Ahmed <naeem.ahmed@portonics.com>
2021-09-05 22:21:25 +0600

    5th_sep_2021

---
+++ _next/static/webpack/pages/index.a184b4c33621aa1d43c7.hot-update.js
@@ -0,0 +1,79 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./utils/server.js":
+/*!*************************!*\
+  !*** ./utils/server.js ***!
+  \*************************/
+/*! exports provided: BASE_API_URL, ZIP_CODE_SECRET, DUMMY_URL */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "BASE_API_URL", function() { return BASE_API_URL; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ZIP_CODE_SECRET", function() { return ZIP_CODE_SECRET; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DUMMY_URL", function() { return DUMMY_URL; });
+var BASE_API_URL = "https://stg-api.culturize.com/api/v1";
+var ZIP_CODE_SECRET = "hvJddKWRJxSitmEqztn1R6d8gu41iKmlYEAj4gTXaqjiTyZsWx6XxQHDRLx42UCm";
+var DUMMY_URL = "https://culturize.s3.amazonaws.com/client-images/53bacf26-00a5-4c31-a81e-0591deba218c.PNG";
+
+;
+    var _a, _b;
+    // Legacy CSS implementations will `eval` browser code in a Node.js context
+    // to extract CSS. For backwards compatibility, we need to check we're in a
+    // browser context before continuing.
+    if (typeof self !== 'undefined' &&
+        // AMP / No-JS mode does not inject these helpers:
+        '$RefreshHelpers$' in self) {
+        var currentExports = module.__proto__.exports;
+        var prevExports = (_b = (_a = module.hot.data) === null || _a === void 0 ? void 0 : _a.prevExports) !== null && _b !== void 0 ? _b : null;
+        // This cannot happen in MainTemplate because the exports mismatch between
+        // templating and execution.
+        self.$RefreshHelpers$.registerExportsForReactRefresh(currentExports, module.i);
+        // A module can be accepted automatically based on its exports, e.g. when
+        // it is a Refresh Boundary.
+        if (self.$RefreshHelpers$.isReactRefreshBoundary(currentExports)) {
+            // Save the previous exports on update so we can compare the boundary
+            // signatures.
+            module.hot.dispose(function (data) {
+                data.prevExports = currentExports;
+            });
+            // Unconditionally accept an update to this module, we'll check if it's
+            // still a Refresh Boundary later.
+            module.hot.accept();
+            // This field is set when the previous version of this module was a
+            // Refresh Boundary, letting us know we need to check for invalidation or
+            // enqueue an update.
+            if (prevExports !== null) {
+                // A boundary can become ineligible if its exports are incompatible
+                // with the previous exports.
+                //
+                // For example, if you add/remove/change exports, we'll want to
+                // re-execute the importing modules, and force those components to
+                // re-render. Similarly, if you convert a class component to a
+                // function, we want to invalidate the boundary.
+                if (self.$RefreshHelpers$.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)) {
+                    module.hot.invalidate();
+                }
+                else {
+                    self.$RefreshHelpers$.scheduleUpdate();
+                }
+            }
+        }
+        else {
+            // Since we just executed the code for the module, it's possible that the
+            // new exports made it ineligible for being a boundary.
+            // We only care about the case when we were _previously_ a boundary,
+            // because we already accepted this update (accidental side effect).
+            var isNoLongerABoundary = prevExports !== null;
+            if (isNoLongerABoundary) {
+                module.hot.invalidate();
+            }
+        }
+    }
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vdXRpbHMvc2VydmVyLmpzIl0sIm5hbWVzIjpbIkJBU0VfQVBJX1VSTCIsIlpJUF9DT0RFX1NFQ1JFVCIsIkRVTU1ZX1VSTCJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQU8sSUFBTUEsWUFBWSxHQUFHLHNDQUFyQjtBQUNBLElBQU1DLGVBQWUsR0FBQyxrRUFBdEI7QUFDQSxJQUFNQyxTQUFTLEdBQUUsMkZBQWpCIiwiZmlsZSI6InN0YXRpYy93ZWJwYWNrL3BhZ2VzL2luZGV4LmExODRiNGMzMzYyMWFhMWQ0M2M3LmhvdC11cGRhdGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgQkFTRV9BUElfVVJMID0gXCJodHRwczovL3N0Zy1hcGkuY3VsdHVyaXplLmNvbS9hcGkvdjFcIjtcclxuZXhwb3J0IGNvbnN0IFpJUF9DT0RFX1NFQ1JFVD1cImh2SmRkS1dSSnhTaXRtRXF6dG4xUjZkOGd1NDFpS21sWUVBajRnVFhhcWppVHlac1d4Nlh4UUhEUkx4NDJVQ21cIlxyXG5leHBvcnQgY29uc3QgRFVNTVlfVVJMID1cImh0dHBzOi8vY3VsdHVyaXplLnMzLmFtYXpvbmF3cy5jb20vY2xpZW50LWltYWdlcy81M2JhY2YyNi0wMGE1LTRjMzEtYTgxZS0wNTkxZGViYTIxOGMuUE5HXCIiXSwic291cmNlUm9vdCI6IiJ9
